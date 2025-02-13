d9ed08ab6c05dd9c020167eecabbc747f213f73a
Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2025-02-13 15:01:55 +0400

    Reinitialzed Project

--- src/components/formComponents/frmSelect.jsx
+++
@@ -1,8 +0,0 @@
-export default function FormSelection({name,value,onChange,children,title,readOnly, disabled=false}){
-     return <div className={`frmControl ${disabled ? 'disabled' : ''}`.trim()}>
-          <label htmlFor={name}>{title}</label>
-          <select name={name} id={name} value={value} onChange={onChange} readOnly={readOnly}>
-               {children}
-          </select>
-     </div>
-}
